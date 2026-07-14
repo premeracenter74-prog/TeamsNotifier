7942af495bf1ff84a04696712c2a7d18c9a7aaff
premeracenter74-prog <premeracenter74@gmail.com>
2026-07-14 10:33:51 +0500

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,29 @@
 using System.Windows;
+using TeamsNotifier.Services;
 
 namespace TeamsNotifier;
 
 public partial class MainWindow : Window
 {
+    private readonly ChromeService _chrome = new();
+
     public MainWindow()
     {
         InitializeComponent();
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var tab = await _chrome.FindTeamsTabAsync();
+
+        if (tab == null)
+        {
+            MessageBox.Show("Teams не найден");
+            return;
+        }
+
+        MessageBox.Show(tab.Title);
     }
 }
